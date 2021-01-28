9f14c9968fd303b908e4fe3c6b9bc2618224ba6c
Ian Saucy <iansaucy@gmail.com>
2021-01-28 10:54:58 -0500

    Small update

--- backend/main.py
+++ backend/main.py
@@ -55,7 +55,3 @@ def create_item_for_user(
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
-@app.get("/items/", response_model=List[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
